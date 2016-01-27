c03e4e50ba1716c1f283d2c54feb772c710bd86e
talum <tracidini@gmail.com>
2016-01-26 20:27:48 -0500

    Refactor JS functions

---
+++ loadData.js
@@ -0,0 +1,66 @@
+$(document).on("ready", function(){
+  var app = new App();
+  app.parseBoroughs();
+  app.displayAllBoroughs();
+  app.neighbrhoodClickListener();
+});
+
+var App = function(){};
+
+App.prototype.parseBoroughs = function(){
+  data.forEach(function(boroughObject){
+      var boroughName = boroughObject.borough.name;
+      var boroughId = boroughObject.borough.id;
+      var mappings = boroughObject.mappings;
+
+      function displayBorough(){
+        $("#container-A").append('<div id="' + boroughId +'"><h1>' + boroughName + '</h1>');
+        } 
+      displayBorough(); 
+      
+      mappings.forEach(function(mappingObject){
+        var macroName = mappingObject.macro.name;
+        var macroId = mappingObject.macro.id;
+
+        var neighborhoods = mappingObject.neighborhoods;
+        var neighborhoodArray = neighborhoods.map(function(obj){
+            return obj.name;
+        });
+       
+       displayMacro();
+
+       function displayMacro(){
+          if (!(neighborhoodArray.includes(macroName))){
+            $("#" + boroughId).append("<h2>" + macroName + "</h2>");
+          }
+          $("#" + boroughId).append('<ul id="' + macroId + '"></ul>');
+        }
+
+        function displayNeighborhood(){
+          for (var i=0; i<neighborhoodArray.length; i++){
+            $("#" + macroId).append("<li><a href='#'>" + neighborhoodArray[i] + "</a></li>");
+          }
+        }
+          displayNeighborhood();   
+      });
+    });  
+}
+
+App.prototype.displayAllBoroughs = function(){
+  data.forEach(function(boroughObject){
+    var boroughId = boroughObject.borough.id;
+    $(".borough-list").append("<li class='" + boroughId + "'>" + boroughObject.borough.name + "</li>");
+  });  
+}
+
+App.prototype.neighbrhoodClickListener = function(){
+   $("li").on("click", function(e){
+      var boroughId = $(this).parent().parent().attr('id');
+      var $borough = $("li."+boroughId);
+      e.preventDefault();
+      $borough.addClass("highlighted");
+      $(this).on('mouseout', function(){
+        $borough.removeClass("highlighted");
+      })
+    });  
+ }
